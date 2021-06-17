4e2c6a9cf99992fa1c6585a863f3d06351086bef
Vishwas <vishwas.g5@gmail.com>
2021-06-17 22:23:04 +0530

    Typing array

---
+++ react-typescript-demo/src/components/props/PersonList.tsx
@@ -0,0 +1,20 @@
+type PersonListProps = {
+  names: {
+    first: string
+    last: string
+  }[]
+}
+
+export const PersonList = (props: PersonListProps) => {
+  return (
+    <div>
+      {props.names.map(name => {
+        return (
+          <h2>
+            {name.first} {name.last}
+          </h2>
+        )
+      })}
+    </div>
+  )
+}
