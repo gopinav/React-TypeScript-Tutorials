d6e93698eeb47903120555de6023b591be3ffccb
Vishwas <vishwas.g5@gmail.com>
2021-06-21 19:18:52 +0530

    Optional prop

--- react-typescript-demo/src/components/props/Greet.tsx
+++ react-typescript-demo/src/components/props/Greet.tsx
@@ -1,15 +1,16 @@
 type GreetProps = {
   name: string
-  messageCount: number
+  messageCount?: number
   isLoggedIn: boolean
 }
 
 export const Greet = (props: GreetProps) => {
+  const { messageCount = 0 } = props
   return (
     <div>
       {props.isLoggedIn ? (
         <h2>
-          Hey {props.name}! You have {props.messageCount} unread messages
+          Hey {props.name}! You have {messageCount} unread messages
         </h2>
       ) : (
         <h2>Welcome Guest</h2>
