8df221e16a883c34590d5f4194170220f265f6dc
Vishwas <vishwas.g5@gmail.com>
2021-06-22 21:42:22 +0530

    Add key prop

--- react-typescript-demo/src/components/props/PersonList.tsx
+++ react-typescript-demo/src/components/props/PersonList.tsx
@@ -10,7 +10,7 @@ export const PersonList = (props: PersonListProps) => {
     <div>
       {props.names.map(name => {
         return (
-          <h2>
+          <h2 key={name.first}>
             {name.first} {name.last}
           </h2>
         )
