8fab470dc59a60ae0c2a0928356cf6367c36d005
Vishwas <vishwas.g5@gmail.com>
2021-07-02 23:25:37 +0530

    Strict action types

--- react-typescript-demo/src/components/state/Counter.tsx
+++ react-typescript-demo/src/components/state/Counter.tsx
@@ -4,10 +4,16 @@ type CounterState = {
   count: number
 }
 
-type CounterAction = {
-  type: string
+type UpdateAction = {
+  type: 'increment' | 'decrement'
   payload: number
 }
+
+type ResetAction = {
+  type: 'reset'
+}
+
+type CounterAction = UpdateAction | ResetAction
 
 const initialState = { count: 0 }
 
@@ -17,6 +23,8 @@ function reducer(state: CounterState, action: CounterAction) {
       return { count: state.count + action.payload }
     case 'decrement':
       return { count: state.count - action.payload }
+    case 'reset':
+      return initialState
     default:
       return state
   }
@@ -33,6 +41,7 @@ export const Counter = () => {
       <button onClick={() => dispatch({ type: 'decrement', payload: 10 })}>
         Decrement 10
       </button>
+      <button onClick={() => dispatch({ type: 'reset' })}>Reset</button>
     </>
   )
 }
