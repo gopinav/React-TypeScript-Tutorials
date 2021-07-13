f02331a2ee6d2fc06d3832aa1c817c079b643429
Vishwas <vishwas.g5@gmail.com>
2021-07-13 18:27:27 +0530

    Remove index

--- react-typescript-demo/src/components/generics/List.tsx
+++ react-typescript-demo/src/components/generics/List.tsx
@@ -10,7 +10,7 @@ export const List = <T extends { id: number }>({
   return (
     <div>
       <h2>List of items</h2>
-      {items.map((item, index) => {
+      {items.map(item => {
         return (
           <div key={item.id} onClick={() => onClick(item)}>
             {item}
